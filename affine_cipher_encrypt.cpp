28b9b82b6cbd116ef12a2f9e0d62de90bf287b5c
Mrinal Sinha <themrinalsinha@gmail.com>
2016-07-31 16:13:26 +0530

    Affine Cipher Encryption and Decryption

---
+++ affine_cipher_encrypt.cpp
@@ -0,0 +1,44 @@
+/*
+* Author : Mrinal Sinha
+* Program : Affine Cipher Encryption algorithm.
+*/
+
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+//Function to calculate GCD.
+int gcd(int x, int y)
+{
+    if(x % y == 0)
+        return y;
+    else
+        return gcd(y,x%y);
+}
+
+main(void)
+{
+    char str[100], cipher_str[100];
+    int a,b;
+    cout << "\n\nEnter String to Encrypt : "; cin >> str;
+    there:
+    cout << "Enter value for 'a' : "; cin >> a;
+
+    //Checking if gcd of a and 26 is 1 or not.
+    int gcd_val = gcd(a,26);
+    cout << "GCD is : " << gcd_val <<"\n";
+    if(gcd_val != 1)
+        goto there;
+    else
+    {
+        cout << "Enter value for 'b' : "; cin >> b;
+        for(int i = 0; i < strlen(str); i++)
+        {
+            str[i] = tolower(str[i]);
+            int val = str[i] - 97;
+            int res = ((a * val + b) % 26) + 97;
+            cipher_str[i] = res;
+        }
+        cout << "Encrypted String is : " << cipher_str <<"\n\n";
+    }
+}
